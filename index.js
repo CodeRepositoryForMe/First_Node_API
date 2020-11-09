79e491813799fa646b64ebab754fc0dd0f7667a0
SujayM <SujayMane@Hotmail.com>
2020-11-09 13:25:45 +0530

    Connection string from Env

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 //Import Route
 const authRoute = require('./route/auth');
 
 // Connect to DB
-mongoose.connect('mongodb+srv://User11:User11@hello.nclx8.mongodb.net/Test?retryWrites=true&w=majority',
+mongoose.connect(
+    process.env.DB_CONNECT,
     {useNewUrlParser: true, useUnifiedTopology: true},
     () => console.log("Connected to DB"));
 
@@ -15,5 +19,5 @@ app.use('/api/user', authRoute);
 
 app.listen(3000, () => console.log("Server Started"));
 
-//DB Connection
+//DB Connection 
 //mongodb+srv://User11:<password>@hello.nclx8.mongodb.net/<dbname>?retryWrites=true&w=majority
